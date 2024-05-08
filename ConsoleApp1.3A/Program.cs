0aef66531d8c4594d6022034f9264e0f3f7cce1f
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-08 16:06:25 -0400

    Add project files.

---
+++ ConsoleApp1.3A/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace ConsoleApp1._3A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] shapes = { "Triangle", "Square", "Rectangle" };
+            Console.WriteLine("What shape are we looking at Triangle, Square, or Rectangle ? ");
+            string shape = Console.ReadLine();
+            
+            Console.WriteLine($"What is the length of the {shape}?");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"What is the width of the {shape}?");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+
+            if (shapes.Contains(shape)) 
+            {
+                Console.WriteLine()
+            }
+
+
+            static double Triangle(int length, int width) 
+            { 
+                double area = length * width/2;
+                return area;
+            }
+            static int Square(int length, int width)
+            {
+                int area = length * width;
+                return area;
+            }
+            static int Rectangle(int length, int width) 
+            {
+                int area = length * width;
+                return area;
+            }
+        }   
+
+    }
+}
